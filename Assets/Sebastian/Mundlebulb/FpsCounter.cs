5b11cba6d5684855f8dfb34a6bdf9fc21c5ad62f
Alexey Shutov <a_sh@phygitalism.com>
2020-10-10 10:35:18 +0300

    added Sebastian's code

---
+++ Assets/Sebastian/Mundlebulb/FpsCounter.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class FpsCounter : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _fpsText;
+    [SerializeField] private float _hudRefreshRate = 1f;
+ 
+    private float _timer;
+ 
+    private void Update()
+    {
+        if (Time.unscaledTime > _timer)
+        {
+            int fps = (int)(1f / Time.unscaledDeltaTime);
+            _fpsText.text = "FPS: " + fps;
+            _timer = Time.unscaledTime + _hudRefreshRate;
+        }
+    }
+}
